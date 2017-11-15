11bfca97820e3bf2c51a09c0b2f6bfdc89bb988f
Daniel Oviedo <daniel.oviedo.rios@gmail.com>
2017-11-15 13:43:02 -0700

    Database updated

--- database/reset.sql
+++ database/reset.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 15, 2017 at 08:18 PM
+-- Generation Time: Nov 15, 2017 at 09:42 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.0.21
 
@@ -21,6 +21,17 @@ SET time_zone = "+00:00";
 --
 -- Database: `reset`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `po`
+--
+
+CREATE TABLE `po` (
+  `id` smallint(5) UNSIGNED NOT NULL,
+  `requisition` smallint(5) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Purchase Orders';
 
 -- --------------------------------------------------------
 
@@ -43,11 +54,41 @@ CREATE TABLE `quotations` (
 --
 
 INSERT INTO `quotations` (`id`, `customer`, `part`, `qty`, `cost`, `total`, `date`) VALUES
-(1, 'Cliente 1', 'Parte 1', 1.5, 10, 15, '2017-11-22');
+(1, 'Cliente 1', 'Parte 1', 1.5, 10, 15, '2017-11-22'),
+(2, 'C1', 'P1', 1.5, 10, 15, '2017-11-22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requisitions`
+--
+
+CREATE TABLE `requisitions` (
+  `id` tinyint(3) UNSIGNED NOT NULL,
+  `quotation` smallint(5) UNSIGNED NOT NULL,
+  `approve1` tinyint(1) NOT NULL,
+  `approve2` tinyint(1) NOT NULL,
+  `approve3` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Quotations';
+
+--
+-- Dumping data for table `requisitions`
+--
+
+INSERT INTO `requisitions` (`id`, `quotation`, `approve1`, `approve2`, `approve3`) VALUES
+(1, 5, 1, 1, 1),
+(2, 0, 0, 0, 0),
+(3, 1, 0, 0, 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `po`
+--
+ALTER TABLE `po`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `quotations`
@@ -56,14 +97,30 @@ ALTER TABLE `quotations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `requisitions`
+--
+ALTER TABLE `requisitions`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `po`
+--
+ALTER TABLE `po`
+  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `quotations`
 --
 ALTER TABLE `quotations`
-  MODIFY `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+  MODIFY `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `requisitions`
+--
+ALTER TABLE `requisitions`
+  MODIFY `id` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
